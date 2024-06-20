5111f890d7d11074394d6e89a7596ed9a14f2678
Igor Leonovich <8408481+igorleonovich@users.noreply.github.com>
2024-06-20 16:20:32 +0200

    Add proxy_pass

--- redirect-docker/nginx-boot.sh
+++ redirect-docker/nginx-boot.sh
@@ -21,12 +21,33 @@ events {
 
 http {
     server {
+
+        # Old part
+
         listen $HTTP_PORT;
         server_tokens off;
         $([ "${HSTS}" != "0" ] && echo "
         add_header Strict-Transport-Security \"max-age=${HSTS_MAX_AGE};$([ "${HSTS_INCLUDE_SUBDOMAINS}" != "0" ] && echo "includeSubDomains")\";
         ")
-        rewrite ^(.*) $REDIRECT\$1 $REDIRECT_TYPE;
+#        rewrite ^(.*) $REDIRECT\$1 $REDIRECT_TYPE;
+
+        # New part
+
+        rewrite ^/(.*)$ /$1 break;
+        proxy_pass https://leonardo.ai;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        proxy_pass_request_headers on;
+        client_max_body_size 100M;
+
+        proxy_http_version 1.1;
+        proxy_set_header Connection "keep-alive";
+        proxy_buffering off;
+        proxy_request_buffering off;
+
     }
 }
 
